a884cc51d8e28948c5b8d37ba73cd01a5069b0c5
sirjudge <nico.a.judge@gmail.com>
2018-08-02 23:01:11 -0400

    added basic commands

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import re
+
 __numPlayers = 2
 __turnNum = 0
+__player = []
 
 if __name__ == "__main__":
     # set number of players and check to see if user input a real number
     numPlayersInput  = input("How many players are there? press enter to default to two players\n")
+
+    # Make sure user input is valid
     try:
         numPlayers = int(numPlayersInput)
     except ValueError:
        print("That's not an int!")
        numPlayers = input('please enter a real number between 1 and 4\n')
+
+    # set global variables accordingly
     if numPlayers == '':
         __numPlayers=2
     elif numPlayers > 4:
@@ -16,14 +23,44 @@ if __name__ == "__main__":
        __numPlayers=4
     else:
         __numPlayers = numPlayers
-    print("welcome to the show\n")
+
+    # initialize player array
+    for x in __numPlayers:
+        __player.additem('player' + x)
+        print("welcome to the show\n")
 
 def startGame():
-    playerTurn = 1 
+    playerTurn = 1
     # In order to stop playing set stillPlaying to false
     stillPlaying = True
     while stillPlaying:
-        
+        currPlayer = player[playerTurn]
+        readCommands(currPlayer)
+        if playerTurn == __numPlayers:
+            playerTurn = 1
+        else:
+            playerTurn += 1 
+
+
+def readCommands(player):
+    turnInProgress = True
+    while(turnInProgress):
+        command = raw_input('What do you want to do?')
+        if command == 'exit': 
+            print( 'are you sure you want to finish your turn?')
+            input = raw_input("yes or no?")
+            if input.lower() == 'yes' or input.lower() == 'y':
+                turnInProgress = False
+            else:
+                print('continuing turn')
+        elif command == 'trade':
+            playerToTrade = raw_input('What player do you want to trade with?')
+            amountToTrade = raw_input('How much do you want o trade?')
+            player.tradeMoney(playerToTrade,amountToTrade)
+        elif command == 'help':
+            print('please use the following commands')
+        else:
+            print('command not recognized')
 
 class player():
     def __init__(self, currMoney, team):
