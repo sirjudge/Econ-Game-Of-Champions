5df99d1836fd3d700387e4552dbe70e4b97d3d25
sirjudge <nico.a.judge@gmail.com>
2018-08-03 21:07:16 -0400

    set up commands, added some comments explaining methods, added getters/setters

--- main.py
+++ main.py
@@ -4,16 +4,101 @@ __numPlayers = 2
 __turnNum = 0
 __player = []
 
+def startGame():
+    playerTurn = 0
+    # In order to stop playing set stillPlaying to false
+    stillPlaying = True
+    while stillPlaying:
+        currPlayer = __player[playerTurn]
+        readCommands(currPlayer)
+        if playerTurn == __numPlayers:
+            playerTurn = 1
+        else:
+            playerTurn += 1
+        print('your turn is over' + currPlayer.getName())
+
+# command reading
+def readCommands(player):
+    turnInProgress = True
+    while(turnInProgress):
+        # temporarily store the current players stats
+        playerName = player.getName()
+        playerMoney = player.getMoney()
+        playerTeamName = player.getTeam()
+
+        # Read Command line input
+        command = input('What do you want to do?')
+
+        # Exit condition, confirm on exit
+        if command == 'exit':
+            print( 'are you sure you want to finish your turn?')
+            commandInput = input("yes or no?")
+            if commandInput.lower() == 'yes' or commandInput.lower() == 'y':
+                turnInProgress = False
+            else:
+                print('continuing turn')
+        elif commandInput == 'trade':
+            playerToTrade = input('What player do you want to trade with?')
+            amountToTrade = input('How much do you want o trade?')
+            player.tradeMoney(playerToTrade,amountToTrade)
+        elif commandInput == 'help':
+            print('please use the following commands')
+        else:
+            print('command not recognized')
+
+class Player():
+    # Global class variables
+    _name = ''
+    _team = ''
+    _money = 0
+
+    def __init__(self,playerName, currMoney,team):
+        self._name = playerName
+        self._money= currMoney
+        self._team = team
+
+    #Getters and setters
+    def getTeam(self):
+        return self._team
+    def setTeam(teamVal):
+        self._team = teamVal
+    def getMoney(self):
+        return self._money
+    def setMoney(self, moneyVal):
+        self._money = moneyVal
+    def getName(self):
+        return self._name
+    def setName(self,name):
+        self._name = name
+
+    #Trading with other players
+    def subtractMoney(self, value):
+        self.setMoney(self.getMoney() - value)
+    def addMoney(self, value):
+        self.setMoney(self.getMoney + value)
+    def tradeMoney(self,playerToTrade, amountToTrade):
+        if self.getMoney() > amountToTrade:
+            self.subtractMoney(amountToTrade)
+            playerToTrade.addMoney(amountToTrade)
+            print(str(amountToTrade) + ' has been traded to ' + playerToTrade)
+        else:
+            print('you do not have enough money in the bank to finish the transaction')
+
+
 if __name__ == "__main__":
     # set number of players and check to see if user input a real number
     numPlayersInput  = input("How many players are there? press enter to default to two players\n")
 
-    # Make sure user input is valid
-    try:
-        numPlayers = int(numPlayersInput)
-    except ValueError:
-       print("That's not an int!")
-       numPlayers = input('please enter a real number between 1 and 4\n')
+    # while intCheck is true, no valid input has been given 
+    intCheck = True
+    while intCheck:
+        # Make sure user input is valid
+        try:
+            numPlayers = int(numPlayersInput)
+            intCheck = False
+        except ValueError:
+           print("That's not an int!")
+           numPlayers = input('please enter a real number between 1 and 4\n')
 
     # set global variables accordingly
     if numPlayers == '':
@@ -25,69 +110,12 @@ if __name__ == "__main__":
         __numPlayers = numPlayers
 
     # initialize player array
-    for x in __numPlayers:
-        __player.additem('player' + x)
-        print("welcome to the show\n")
-
-def startGame():
-    playerTurn = 1
-    # In order to stop playing set stillPlaying to false
-    stillPlaying = True
-    while stillPlaying:
-        currPlayer = player[playerTurn]
-        readCommands(currPlayer)
-        if playerTurn == __numPlayers:
-            playerTurn = 1
-        else:
-            playerTurn += 1 
-
-
-def readCommands(player):
-    turnInProgress = True
-    while(turnInProgress):
-        command = raw_input('What do you want to do?')
-        if command == 'exit': 
-            print( 'are you sure you want to finish your turn?')
-            input = raw_input("yes or no?")
-            if input.lower() == 'yes' or input.lower() == 'y':
-                turnInProgress = False
-            else:
-                print('continuing turn')
-        elif command == 'trade':
-            playerToTrade = raw_input('What player do you want to trade with?')
-            amountToTrade = raw_input('How much do you want o trade?')
-            player.tradeMoney(playerToTrade,amountToTrade)
-        elif command == 'help':
-            print('please use the following commands')
-        else:
-            print('command not recognized')
-
-class player():
-    def __init__(self, currMoney, team):
-        self.currentMoney= currMoney
-        self.team = team
-
-    def getTeam(self):
-        return self.team
-    def setTeam(teamVal):
-        self.team = teamVal
-
-    def getMoney(self):
-        return self.currentMoney
-    def setMoney(self, moneyVal):
-        self.currentMoney = moneyVal
-
-    def subtractMoney(self, value):
-        self.setMoney(self.getMoney() - value)
-
-    def addMoney(self, value):
-        self.setMoney(self.getMoney + value)
-
-    def tradeMoney(self,playerToTrade, amountToTrade):
-        if self.getMoney() > amountToTrade:
-            self.subtractMoney(amountToTrade)
-            playerToTrade.addMoney(amountToTrade)
-            print(str(amountToTrade) + ' has been traded to ' + playerToTrade)
-        else:
-            print('you do not have enough money in the bank to finish the transaction')
-
+    for x in range(0,__numPlayers):
+        # Set the player and team name
+        playerName = input('what is your name, player ' + str(x) + '?\n')
+        teamName = input('what is your team name ' + playerName  + '?\n')
+        #add new player object to the player array
+        newPlayer = Player(playerName, 100, teamName)
+        __player.append(newPlayer)
+        print("welcome to the game " + playerName + '. You are with team ' + teamName)
+    startGame()
